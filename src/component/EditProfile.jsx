13194c3e9801e10dc3ff59e006712822e7d8e1d4
Uttam Kumar <uttamraaz7177@gmail.com>
2025-01-24 00:36:52 +0530

    fixed profile component

--- src/component/EditProfile.jsx
+++ src/component/EditProfile.jsx
@@ -197,7 +197,7 @@ const EditProfile = ({ user }) => {
 
         {/* User Card */}
         <div className="flex justify-center">
-          <div className="card bg-base-300 w-[430px] max-w-full shadow-xl">
+          <div className="card  w-[430px] max-w-full shadow-xl">
             <UserCard
               user={{ firstName, lastName, photoUrl, age, gender, about, skills, college }}
             />
